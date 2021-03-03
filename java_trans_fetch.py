a4b9ba1b9a8caf05e8cff13231a48d15d249097c
Jullan-M <jmihkkal_lindi@hotmail.com>
2021-03-03 18:08:13 +0100

    Added more methods to language comparisons

--- java_trans_fetch.py
+++ java_trans_fetch.py
@@ -24,6 +24,11 @@ class Locale(dict):
             base[k] = ""
         dict2lang(base, out_file)
     
+    def merge(self, other):
+        for k in other:
+            if not (k in self):
+                self[k] = other[k]
+    
     def to_json(self, filename: str):
         dict2json(self, filename)
 
@@ -31,35 +36,42 @@ class Locale(dict):
         dict2lang(self, filename)
 
 class Locale_Pair:
+    
     def __init__(self, loc_L: Union[dict, Locale], loc_R: Union[dict, Locale], existing_keys: list = []):
         self.loc_L = loc_L if type(loc_L) == Locale else Locale(loc_L)
         self.loc_R = loc_R if type(loc_R) == Locale else Locale(loc_R)
-        self.corresponding, self.missing = self.corresponding_keys(existing_keys)
+        #self.count = 0
+        self.corresponding = self.corresponding_keys(existing_keys)
     
-    def corresponding_keys(self, existing_keys: list = []) -> (dict, list):
+    def corresponding_keys(self, existing_keys: list) -> (dict, list):
         corr = {} # Corresponding keys between loc_L and loc_R
-        missing = [] # Keys that exist in loc_R but are missing in existing_keys
-        if existing_keys:
-            for k1, v1 in self.loc_L.items():
-                for k2, v2 in self.loc_R.items():
-                    if v1 == v2 and not (k2 in missing):
-                        if k2 in existing_keys:
-                            corr[k1] = k2
-                        else:
-                            missing.append(k2)
-                    elif v1.lower() == v2.lower():
-                        if k2 in existing_keys:
-                            corr[k1] = k2
-                        else:
-                            missing.append(k2)
-                        
-        else:
-            for k1, v1 in self.loc_L.items():
-                locR_vals = list(self.loc_R.values())
-                if v1 in locR_vals:
-                    i = locR_vals.index(v1)
-                    corr[k1] = list(self.loc_R.keys())[i]
-        return corr, missing
+        ignore_chars = dict.fromkeys(map(ord, " ,.!?-:;'"), None)
+        
+        for k1, v1 in self.loc_L.items():
+            for k2 in existing_keys:
+                v2 = self.loc_R[k2]
+                if v1 == v2:
+                    corr[k1] = k2
+                elif v1.lower() == v2.lower():
+                    corr[k1] = k2
+                elif k1==k2:
+                    corr[k1] = k2
+                
+                elif len(v1) > 5 and len(v2) > 5:
+                    strip1 = v1.translate(ignore_chars)
+                    strip2 = v2.translate(ignore_chars)
+                    if strip1.lower() == strip2.lower():
+                        print(v1)
+                        print(v2)
+                        corr[k1] = k2
+                """
+                elif len(v1) > 25 and len(v2) > 25 and lcs(v1, v2, False) >= 0.5*(len(v1)+len(v2))/2:
+                    print(v1)
+                    print(v2)
+                    dict_append(k1, k2)
+                """
+        
+        return corr
 
         
     def translate_corr_entries(self, localization: Union[dict, Locale], temp_file: str = "") -> dict:
@@ -70,17 +82,45 @@ class Locale_Pair:
                 translated[kc] = loc[self.corresponding[kc]]
         return translated
 
+def generate_tranlation(locale_pair_files: list, dest_locale_file: str):
+    ext1 = locale_pair_files[0][0].split(".")[-1].lower()
+    ext2 = locale_pair_files[0][1].split(".")[-1].lower()
+    dest_locale = file2dict(dest_locale_file)
+    unused_keys = set(dest_locale.keys())
+    translation = {}
+    for loc_L, loc_R in locale_pair_files:
+        loc_pair = Locale_Pair(file2dict(loc_L), file2dict(loc_R), dest_locale.keys())
+        loc_trans = loc_pair.translate_corr_entries(dest_locale)
+        count = 0
+        for k in loc_pair.corresponding:
+            if not (k in translation):
+                translation[k] = loc_trans[k]
+                unused_keys.discard(loc_pair.corresponding[k])
+                count+=1
+        print(count, "entries translated from pair:", loc_L, loc_R)
+        print("Current unused_keys:", len(unused_keys))
+    dict2file(translation, dest_locale_file.replace(ext2, ext1))
+    return translation
 
 
+loc_par = [["en_US.lang", "en_us.json"], ["fi_FI.lang", "fi_fi.json"], ["nb_NO.lang", "no_no.json"]]
+dest_loc = "se_no.json"
+generated = generate_tranlation(loc_par, dest_loc)
+working = Locale(lang2dict("se_NO_manual.lang"))
+working.merge(generated)
+working.to_lang("se_no_generated.lang")
+"""
 en_bed = lang2dict("en_US.lang")
+en = Locale(en_bed)
+en.generate_lang_template("en_US_template.lang")
 en_java = json2dict("en_us.json")
 se_java = json2dict("se_no.json")
 
 
 bed_java_en = Locale_Pair(en_bed, en_java, se_java.keys())
-translation = bed_java_en.translate_corr_entries(se_java, temp_file="en_US_template.lang")
+translation = bed_java_en.translate_corr_entries(se_java)
 dict2lang(translation, "se_no.lang")
-"""
+
 
 dups = loc_bed.keys_with_duplicate_values(list(json2dict("en-bed-java_corresponding_keys.json").keys()))
 dict2json(dups, "duplicates.json")
--- utilities.py
+++ utilities.py
@@ -21,7 +21,7 @@ def lang_parser(lang_txt: str):
     Note: all comments in the .lang file will be stripped out in the result dict.
     """
     # 
-    # This excludes any comments
+    # This excludes any comments¨
     lang_dic = {}
     for line in lang_txt:
         strip = line.strip()
@@ -44,9 +44,9 @@ def lang2dict(filename: str):
 def dict2json(dictionary: dict, out_file: str):
     # Converts a local Python dictionary to a .json with the filename out_file
     with open(out_file, "w", encoding="utf-8") as js:
-        js.write(json.dumps(dictionary, indent="\t"))
+        js.write(json.dumps(dictionary, indent="\t", sort_keys=True))
 
-def dict2lang(dictionary: str, out_file: str):
+def dict2lang(dictionary: dict, out_file: str):
     # Converts a local Python dictionary to a .lang with the filename out_file
     with open(out_file, "w", encoding="utf-8") as lg:
         lg.write("\n".join(["=".join([k, dictionary[k]]) for k in dictionary]))
@@ -54,9 +54,75 @@ def dict2lang(dictionary: str, out_file: str):
 def lang2json(lang_file: str):
     # Converts a .lang to a .json of the same name
     dc = lang2dict(lang_file)
-    dict2json(dc, f"{lang_file.replace('.lang', '')}.json")
+    dict2json(dc, lang_file.replace('.lang', 'json"'))
 
 def json2lang(json_file: str):
     # Converts a .json file to a .lang of the same name
     dc = json2dict(json_file)
-    dict2json(dc, f"{json_file.replace('.json', '')}.lang")
+    dict2lang(dc, json_file.replace('.json', '.lang'))
+
+def file2dict(filename: str):
+    # Converts a file to dictionary based on extension
+    # The file must either be .lang or .json
+    assert ('.' in filename), f"Missing extension in filename: {filename}"
+    ext = filename.split(".")[-1].lower()
+    if ext == "json":
+        return json2dict(filename)
+    elif ext == "lang":
+        return lang2dict(filename)
+    else:
+        raise ValueError(f"Unknown file extension in filename: {filename}")
+
+def dict2file(dictionary: dict, out_file: str):
+    # Converts a .lang to a .json of the same name
+    assert ('.' in out_file), f"Missing extension in filename: {out_file}"
+    ext = out_file.split(".")[-1].lower()
+    if ext == "json":
+        dict2json(dictionary, out_file)
+    elif ext == "lang":
+        dict2lang(dictionary, out_file)
+    else:
+        raise ValueError(f"Unknown file extension in filename: {out_file}")
+
+
+
+def lcs(x: str, y: str, print_out: bool = False): 
+    # find the length of the strings 
+    m = len(x) 
+    n = len(y) 
+  
+    # declaring the array for storing the dp values 
+    ls = [[None]*(n + 1) for i in range(m + 1)] 
+  
+    # Following steps build L[m + 1][n + 1] in bottom up fashion 
+    # Note: L[i][j] contains length of LCS of x[0..i-1] and y[0..j-1]
+    for i in range(m + 1): 
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                ls[i][j] = 0
+            elif x[i-1] == y[j-1]: 
+                ls[i][j] = ls[i-1][j-1]+1
+            else:
+                ls[i][j] = max(ls[i-1][j], ls[i][j-1])
+    
+    if print_out:
+        index = ls[m][n]
+        lcs_str = [""] * (index+1) 
+        i = m
+        j = n
+        while i > 0 and j > 0:
+            # If current character in x and y are same then current character is part of LCS 
+            if x[i-1] == y[j-1]: 
+                lcs_str[index-1] = x[i-1] 
+                i-=1
+                j-=1
+                index-=1
+            # If not same, then find the larger of two and go in the direction of larger value 
+            elif ls[i-1][j] > ls[i][j-1]: 
+                i-=1
+            else: 
+                j-=1
+        print("str1:\t" + x + "\nstr2:\t" + y + "\nlcs:\t" + "".join(lcs_str))
+    
+    # ls[m][n] contains the length of LCS of x[0..n-1] & y[0..m-1]
+    return ls[m][n]
